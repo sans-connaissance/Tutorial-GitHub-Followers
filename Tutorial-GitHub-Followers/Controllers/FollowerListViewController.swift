db14d5714b1bc193788e8ea3de7740610e8fc623
sans-connaissance <davemalicke@gmail.com>
2022-06-18 12:09:27 -0500

    ADD downloadable images to FollowerCell

--- Tutorial-GitHub-Followers/Controllers/FollowerListViewController.swift
+++ Tutorial-GitHub-Followers/Controllers/FollowerListViewController.swift
@@ -9,27 +9,71 @@ import UIKit
 
 class FollowerListViewController: UIViewController {
     
+    enum Section {
+        case main
+    }
+    
     var username: String!
+    var followers: [Follower] = []
+    var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        navigationController?.navigationBar.prefersLargeTitles = true
-        
-        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
-            switch result {
-            case .success(let followers):
-                print(followers)
-            case .failure(let error):
-                self.presentGFAlertOnMainThread(title: "Error", message: error.rawValue, buttonTitle: "Ok")
-            }
-            
-        }
-    
+        configureViewController()
+        configureCollectionView()
+        getFollowers()
+        configureDataSource()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    func configureViewController() {
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func configureCollectionView() {
+        //initialize object
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
+        //use object
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemCyan
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+        
+    }
+    
+    func getFollowers() {
+        NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] result in
+            guard let self = self else {return}
+            
+            switch result {
+            case .success(let followers):
+                self.followers = followers
+                self.updateData()
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Error", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
+    }
+    
+    func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: { collectionView, indexPath, follower -> UICollectionViewCell? in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.reuseID, for: indexPath) as! FollowerCell
+            cell.set(follower: follower)
+            return cell
+        })
+    }
+    
+    func updateData() {
+        var snapShot = NSDiffableDataSourceSnapshot<Section, Follower>()
+        snapShot.appendSections([.main])
+        snapShot.appendItems(followers)
+        DispatchQueue.main.async {
+            self.dataSource.apply(snapShot, animatingDifferences: true)
+        }
     }
 }
--- Tutorial-GitHub-Followers/Views/GFAvatarImageView.swift
+++ Tutorial-GitHub-Followers/Views/GFAvatarImageView.swift
@@ -28,4 +28,24 @@ class GFAvatarImageView: UIImageView {
         
     }
     
+    //place holder images communicate error of not being able to download image
+    //called each time the cell comes on screen -- so there would be too many errors because there are so many calls on this function.
+    func downloadImage(from urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) {[weak self] data, response, error in
+            guard let self = self else {return}
+            if error != nil {return}
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { return }
+            guard let data = data else { return }
+            
+            guard let image = UIImage(data: data) else { return }
+            DispatchQueue.main.async {
+                self.image = image
+            }
+            
+        }
+        task.resume()
+        
+    }
 }
